328b7e298e0182adcbebd2cb7034d957ffec5ba9
Jeremy Cowgar <jeremy@cowgar.com>
2023-02-04 16:37:56 -0500

    feat: code to make SublimeLinter work with Credo

--- linter.py
+++ linter.py
@@ -1,10 +1,21 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import Linter
+
+# C = Consistency -- 1
+# D = Design -- 2
+# R = Readability -- 4
+# F = Refactoring -- 8
+# W = Warning -- 16
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
-    multiline = False
+class Credo(Linter):
+    cmd = 'mix credo list --format=flycheck --all --all-priorities ${file}'
+
+    regex = (
+        r'^[^:]*:(?P<line>\d+)(:(?P<col>\d+))?: '
+        r'(?:(?P<error>(W))|(?P<warning>(C|D|F|R))): '
+        r'(?P<message>.+)$'
+    )
+
     defaults = {
-        'selector': 'source.python'
+        'selector': 'source.elixir'
     }
